ad2c18d5f2da194764c7cc24e5ecccfe42bb6c0b
Lizzy <osho.passion1@gmail.com>
2026-08-09 17:18:43 +0100

    added rating and price

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
- import Customer from "./components/Customer";
+import Customer from "./components/Customer";
 import type { Product } from "./components/type";
 import Hero from "./components/Hero";
 import axios from "axios";
 import Image from "next/image";
-
+import { Star, StarHalf } from "lucide-react";
 
 export default async function Home() {
   const res = await axios.get("https://dummyjson.com/products");
@@ -11,31 +11,52 @@ export default async function Home() {
   console.log(data);
   return (
     <div>
-    
-
       <Hero />
-      
-        <h2 className="flex justify-center font-extrabold text-[30px] pt-5">OUR PRODUCTS</h2>
-      <div className="grid grid-cols-4 gap-5 mx-auto">
-      
+
+      <h2 className="flex justify-center font-extrabold text-[30px] pt-5">
+        OUR PRODUCTS
+      </h2>
+        <div className="container mx-auto">
+      <div className="grid grid-cols-4 gap-4 w-[90%] mx-auto">
         {data.slice(0).map((item: Product) => {
           return (
             <div key={item.id} className="relative">
               <Image
                 src={item?.images[0]}
-                alt={item.name} 
+                alt={item.name}
                 width={50}
                 height={500}
-                className="w-full h-[270] bg-gray-200 mt-10 rounded-lg ml-6"
+                className="w-full h-[270] bg-gray-200 mt-10 rounded-lg"
               />
-               <p className="absolute -mt-10 pl-10 -bottom-6 text-1xl text-black font-light">
+              <p className="absolute -mt-10 pl-10 -bottom-6 text-1xl text-black font-light">
                 {item.title}
-              </p> 
+              </p>
+
+              <div className="flex items-center justify-center gap-1 text-yellow-300">
+                {[...Array(5)].map((_, i) => {
+                  if (i < Math.floor(item.rating)) {
+                    return <Star key={i} className="w-4 h-4 fill-yellow-300" />;
+                  }
+                  if (i === Math.floor(item.rating) && item.rating % 1 >= 0.5) {
+                    return (
+                      <StarHalf key={i} className="w-4 h-4 fill-yellow-300" />
+                    );
+                  }
+                  // return <Star key={i} className="w-4 h-4 text-gray-300" />
+                })}
+                <span className="text-[8px] text-gray-400 ml-1">
+                  {item.rating.toFixed(1)}
+                </span>
+                <p>${item.price}</p>
+              </div>
             </div>
+            
           );
         })}
+         
       </div>
-        <Customer />
+      </div>
+      <Customer />
     </div>
   );
 }
